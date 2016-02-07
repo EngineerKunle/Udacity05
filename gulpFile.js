0fe3849c8f42a5f3513c07cb3a61b50be93a3a62
Engineer Kunle <kunle.ogunjimi@cohaesus.co.uk>
2016-02-07 21:46:32 +0000

    first stage structure

--- gulpFile.js
+++ gulpFile.js
@@ -1,21 +1,31 @@
 "use strict";
 
-var gulp   = require('gulp'),
-    sass   =  require('gulp-sass');
+var gulp = require('gulp'),
+   sass = require('gulp-sass');
 
 
 //sass compile
-gulp.task('sass', function(){
-    return gulp.src('src/scss/style.scss')
-   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('dist/css'));
+gulp.task('sass', function () {
+   return gulp.src('src/scss/style.scss')
+      .pipe(sass({
+         outputStyle: 'compressed'
+      }).on('error', sass.logError))
+      .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('watch',function(){
+//here is the copy task
+gulp.task('copyJs', function () {
+   return gulp.src(['src/libs/*.js'])
+      .pipe(gulp.dest('dist/libs/'));
+});
+
+//here is the watch task
+
+gulp.task('watch', function () {
    //watch sass files
    gulp.watch('src/scss/*.scss', ['sass']);
-   
+
 });
 
 //task
-gulp.task('default',['sass', 'watch']);
+gulp.task('default', ['sass','copyJS', 'watch']);
